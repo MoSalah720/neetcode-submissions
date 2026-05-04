d655ec3311f19e9b5122797603e18b708a5e9b8d
Mohamed Salah <sm2015846@gmail.com>
2026-05-04 20:22:28 +0300

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.cs
@@ -0,0 +1,17 @@
+public class Solution {
+
+    public bool hasDuplicate(int[] nums) 
+    {
+        HashSet<int> x = new HashSet<int>();
+         for (int i = 0; i < nums.Length ; i++)
+            {
+
+                if (x.Contains(nums[i]))
+                {
+                    return true;
+                }
+                x.Add(nums[i]);
+            }
+        return false;
+    }
+}

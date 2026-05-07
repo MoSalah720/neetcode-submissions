9ac8c310e4f004640b9746f41b66cc62ce940028
Mohamed Salah <sm2015846@gmail.com>
2026-05-07 18:42:03 +0300

    Add: longest-substring-without-duplicates - submission-1

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-1.cs
@@ -0,0 +1,19 @@
+public class Solution {
+    public int LengthOfLongestSubstring(string s) {
+              int l = 0;
+ HashSet<char> set = new HashSet<char>();
+ int MaxLenght = 0;
+ for (int r = 0; r <s.Length; r++)
+ {
+     while (set.Contains(s[r]))
+     {
+         set.Remove(s[l]);
+         l++;
+     }
+     set.Add(s[r]);
+     MaxLenght =Math.Max(MaxLenght, r - l + 1);
+ }
+
+ return MaxLenght;
+    }
+}
